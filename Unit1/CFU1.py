5df2dca09d5b9f05dabc2e6faa6e7a91983b44dc
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2024-09-10 08:51:38 -0400

    Create CFU1.py

---
+++ Unit1/CFU1.py
@@ -0,0 +1,22 @@
+fname = "Jannatul" #String variable
+
+num = 4 #int
+
+decimal = 0.5 #float
+
+fact = True #boolean
+
+print(13) #prnts number
+print("We are the best") #prints string
+print(fname) #prints value of fname
+
+question = input("What day is it?") #whatever teh user answer os store in teh variable
+print(fname, "Happy" + question)
+
+#Date: Jannatul Jami
+#period: 1 and 2
+
+#The Do Now Answer
+question1 = input("What's your first name?")
+question2 = input("What's your last name?")
+print("Hello, " + question1 + " " + question2)
