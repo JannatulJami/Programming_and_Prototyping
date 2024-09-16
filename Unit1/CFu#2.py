49d227e606c4f8edc9a66c21a1788260f231505a
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2024-09-16 09:27:47 -0400

    Create CFu#2.py

---
+++ Unit1/CFu#2.py
@@ -0,0 +1,7 @@
+#Name Jannatul Jami Period 1 and 2 9/16/2024
+first_name = input("What is your first name: ")
+last_name = input("What is your last name: ")
+user_age = input("How old are you: ")
+eating_time = input("How long have you been going to school: ")
+print("Hello " + first_name + " " + last_name + " you're " + user_age + " years old, and have been going to school for "
+      + eating_time + " years")
