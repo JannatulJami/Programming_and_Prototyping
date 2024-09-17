cf251c08f8e84852e78f0156388ab688c65750c7
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2024-09-17 19:57:26 -0400

    Create CFU3.py

---
+++ Unit1/CFU3.py
@@ -0,0 +1,12 @@
+#Jannatul Jami 9/17/2024 Period 1 and 2
+first_name = input("Write your first name: ")
+last_name = input("Write your last name: ")
+
+first_number = int(input("First number pick: "))
+second_number = int(input("Second number pick: "))
+
+print("Hello " + first_name + " " + last_name)
+print(first_number + second_number)
+print(first_number * second_number)
+print(first_number / second_number)
+print(first_number % second_number)
