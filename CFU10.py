8b7f0d516ff27a71ea092520b70f63c76e934e0a
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2024-10-22 20:41:12 -0400

    Update CFU10.py

--- CFU10.py
+++ CFU10.py
@@ -1,17 +1,18 @@
 #Jannatul jami Period 1 and 2 
 import random
-guesses = 0
-user_guess = int(input("Guess a number bewteen 1-6: "))
+score = 0
 number_rolls = int(input("How many rolls do you want to play: "))
 
 def game():
 number = random.randint(1,6)
+user_guess = int(input("Guess a number bewteen 1-6: "))
 print(number)
 attempts += 1 
     if user_guess == number:
-        print("That's correct")
-    elif user_guess > number:
-        print("Too High")
+        print("That's correct you earn +1 point")
+        score += 1
     else:
-        print("Too Low")
-   
+        print("Incorrect! That's one less point
+              score += 1
+game()
+
