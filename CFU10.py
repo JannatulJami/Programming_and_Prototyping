57d5ccb2d06943069eb2820f32a0a8db2b9c7a24
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2024-10-24 11:22:59 -0400

    Update CFU10.py

--- CFU10.py
+++ CFU10.py
@@ -1,16 +1,27 @@
 #Jannatul Jami Period 1 and 2 10/18/2024
-import random
-score = 0
-number_rolls = int(input("How many rolls do you want to play: "))
 
-for roll in range(1, number_rolls + 1 ):
-    number = random.randint(1,6)
-    user_guess = int(input("Guess a number bewteen 1-6: "))
-    print(number)
-    if user_guess == number:
-        print("That's correct you earn +1 point")
-        score += 1
+import random
+print("Let's roll the dice!")
+num_rolls = int(input("How many times do we want to roll the dice?"))
+
+def guess(num_random,total):
+    
+    guess = int(input("Guess the roll: "))
+    if guess == num_random:
+        total = total +6
     else:
-        print("Incorrect! That's one less point")
-        score -= 1
-print("You got " + str(score) + " many points.")
+        total = total -1
+        
+        
+def rolls(num_rolls, total =0):
+    num_random = random.randint(1,6)
+    if num_rolls ==0:
+        return total
+    else:
+        num_rolls = num_rolls -1
+        guess(num_random,total)
+        print(f"you roll a {num_random}!")
+        return rolls(num_rolls,total)
+    
+total =rolls(num_rolls)
+print(f"total is: {total}")
