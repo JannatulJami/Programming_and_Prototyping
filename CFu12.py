ad8ff721aa8d63280673e457595635cb74e37cc9
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2024-10-29 14:19:54 -0400

    Update CFu12.py

--- CFu12.py
+++ CFu12.py
@@ -1,8 +1,11 @@
 #Jannaul Jami 10/29/2024 Period 1 and 2
 
 ask_password = input("Type the password in: ")
-
-while ask_password != "simonnyc":
+guesses = 0
+while ask_password != "simonnyc" and guesses < 3:
     print("Wrong Password")
+    gusses += 1
     ask_password = input("Type the password in: ")
+if guesses >= 3:
+    print("No more guesses")
 print("Correct! You may enter....")
