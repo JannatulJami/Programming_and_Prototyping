8820710e0718acdda27de877123c21a3e8ac3749
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2024-10-29 09:22:13 -0400

    Create CFu12.py

---
+++ CFu12.py
@@ -0,0 +1,8 @@
+#Jannaul Jami 10/29/2024 Period 1 and 2
+
+ask_password = input("Type the password in: ")
+
+while ask_password != "simonnyc":
+    print("Wrong Password")
+    ask_password = input("Type the password in: ")
+print("Correct! You may enter....")
