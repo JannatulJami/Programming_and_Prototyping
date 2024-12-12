b3a640745fcce66ea4041bf4949aadbe32de058d
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2024-12-12 09:22:21 -0500

    Create PSassignment6.py

---
+++ PSassignment6.py
@@ -0,0 +1,33 @@
+#Jannnatul Jami
+#Period 1 and 2 
+#12/12/2024
+
+
+import simplegui
+import random
+snow_y = []
+
+width = 600
+height = 400
+snow_flakes = 5
+snowfall_speed= 1
+
+for i in range(snow_flakes):
+    snow_y.append(random.randint(0, height))
+    
+
+def draw_handler(canvas):
+    global snow_y
+    #Draw the bakground
+    canvas.draw_polygon([(0, 0), (width, 0), 
+                        (width, height), 
+                        (0, height)], 1, "white", "LightBlue")
+    for i in range(snow_flakes):
+        snowflake_x = random.randint(0, width)
+        snowflake_y = random.randint(0, height)
+        canvas.draw_circle((snowflake_x, snow_y[i]), 10, 1, "white", "white")
+                           
+frame = simplegui.create_frame('TWinter animation', width, height)
+frame.set_draw_handler(draw_handler)
+
+frame.start()
