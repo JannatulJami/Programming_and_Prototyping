5f048e7b9c2c2d79052880d77c4b97c2ddb4d2c0
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2025-01-16 18:31:09 -0500

    Update videgame2.py

--- videgame2.py
+++ videgame2.py
@@ -4,13 +4,14 @@ import random
 width = 600
 height= 700
 room1 = True 
-room2 = True
-room3 = True
+room2 = False
+room3 = False
 frame = None
 score = []
 items = []
 character = {"x": width // 2, "y": height - 100, "health": 100, "score": 0}
 twinkle_frame = 0 
+victory = False
 
 def room1_drawing(canvas):
     canvas.draw_polygon([(0, 0), (width, 0), 
@@ -69,6 +70,19 @@ def draw(canvas):
         canvas.draw_circle((x, y), size, 1, "White", "White")
 
     twinkle_frame += 1  # Update counter
+#Draw a charcter
+    canvas.draw_circle((character["x"], character["y"]), 15, 2, "#da9cb6", "#d8a9de")
+def keydown(key):
+    if victory:
+        return
+    if key == simplegui.KEY_MAP["up"] and character["y"] > 15:
+        character["y"] -= 10
+    elif key == simplegui.KEY_MAP["down"] and character["y"] <  height - 15:
+        character["y"] += 10
+    elif key == simplegui.KEY_MAP["left"] and character["x"] <  height - 15:
+        character["x"] -= 10
+    elif key == simplegui.KEY_MAP["right"] and character["x"] <  height - 15:
+        character["x"] += 10
 
 # Generate items and threats
 def generate_objects():
@@ -77,15 +91,21 @@ def generate_objects():
     if random.random() < 0.01: # 1% chance per frame
         threats.append({"x": random.randint(20, width - 20), "y": 0})
 def toggle_room1():
-    global room1
-    room1 = not room1
+    global room1, room2, room3
+    room1 = True
+    room2 = False
+    room3 = False
 
 def toggle_room3():
-    global room3
-    room3 = not room3
+    global room1, room2, room3
+    room3 = True
+    room2 = False
+    room1 = False
 def toggle_room2():
-    global room2
-    room2 = not room2
+    global room2, room1, room3
+    room2 = True
+    room1 = False
+    room3 = False
         
 def create_frame():
     global frame
