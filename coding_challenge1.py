428fd986dc64db3fc280f84b56a0af0536e4fce7
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2025-02-27 09:29:45 -0500

    Update coding_challenge1.py

--- coding_challenge1.py
+++ coding_challenge1.py
@@ -4,24 +4,24 @@
 
 #function for question 1
 import math # To be able to use pi
-def sphere_volume(radius):
-    volume = (4/3) * math.pi *(radius **3)
-    return volume
-volume_reset = sphere_volume(5)
-print(f"The volume of a sphere of radius 5 is {volume_reset:2F}")
+def sphere_volume(radius): 
+    volume = (4/3) * math.pi *(radius **3) #Forula of the volume of a sphere
+    return volume #Return the volume of the sphere
+volume_reset = sphere_volume(5)#Making the radius equal to 5 and trying to run it
+print(f"The volume of a sphere of radius 5 is {volume_reset:.2F}") #Writing the volume output for a radius of 5
 
     
 #function for question 2
-def w_cost(copies):
-    cover_price = 24.95
-    discount = 0.40
-    ship_first = 3
-    ship_after = 0.75
-    discount_price = cover_price* (1- discount)
-    total_book_cost = discount_price * copies
-    ship_cost = ship_first + (ship_after * (copies - 1))
-    total = ship_cost + total_book_cost 
-    print(f"Your total is ${total :2f}") # To print the final sentence with the total cost
+def w_cost(copies): #For users to put whatever number of copies they want
+    cover_price = 24.95 #The cover price for one book
+    discount = 0.40 #The discount amount
+    ship_first = 3 #The shipping value cost of the first copy
+    ship_after = 0.75 ##The shipping cost value of everyother copy after the forst one
+    discount_price = cover_price* (1- discount) #Finding the cost after the dscount price was added
+    total_book_cost = discount_price * copies #finding the cost of all the copies
+    ship_cost = ship_first + (ship_after * (copies - 1)) #finding the total shipping cost
+    total = ship_cost + total_book_cost #
+    print(f"Your total is ${total :.2f}") # To print the final sentence with the total cost
 w_cost(60) #To play the function
 
 
@@ -31,16 +31,16 @@ w_cost(60) #To play the function
 
 # Function for question 3
 def running_time(start_hour, start_minutes):
-    easy_pace = 8*60 +15
-    temp_pace = 7*60+12
+    easy_pace = 8*60 +15 #How long it takes in seconds to run one mile in easy pace
+    temp_pace = 7*60+12 #How long it takes in seconds to run one mile in tempo pace
     
     
-    total_time_seconds = easy_pace * 2 + (temp_pace *3)
-    total_minutes = total_time_seconds // 60
-    total_seconds = total_time_seconds % 60
+    total_time_seconds = easy_pace * 2 + (temp_pace *3) #Total time in seconds to run 2 miles in easy pace and 3 miles in tempo pace
+    total_minutes = total_time_seconds // 60 #The number of minutes of the total running time
+    total_seconds = total_time_seconds % 60 #The number of seconds remaining after calculating the seconds
     
-    end_minutes = total_minutes + start_minutes
-    end_hour = start_hour
+    end_minutes = total_minutes + start_minutes #Finding the number of minutes that passed after 6:52 am
+    end_hour = start_hour #
     
     if end_minutes >= 60:
         end_hour += end_minutes // 60
@@ -48,5 +48,4 @@ def running_time(start_hour, start_minutes):
     return end_hour, end_minutes
 end_hour, end_minutes = running_time(6, 52)
 print(f"You will arrive home for breakfast at {end_hour}:{end_minutes} AM.")
-    
     
