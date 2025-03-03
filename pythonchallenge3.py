e1185b2251cfa2e5872d437d2c1450d428d819b0
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2025-03-03 08:37:08 -0500

    Create pythonchallenge3.py

---
+++ pythonchallenge3.py
@@ -0,0 +1,15 @@
+#Jannatul Jami
+#3/3/225
+sentence = input("Input your string: ")
+def print_twice(string):
+    print(string)
+    print(string)
+    
+def do_twice(spam):
+    print(spam)
+    print(spam)
+def do_four(spam):
+    do_twice(spam)
+    do_twice(spam)
+    
+do_four(print_twice(sentence))
