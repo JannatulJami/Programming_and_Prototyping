350a0ca01d3526b11063f928dc48f5f5f323faf5
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2025-03-19 04:53:31 -0400

    Create codingchallenge9.py

---
+++ codingchallenge9.py
@@ -0,0 +1,46 @@
+#Jannatul Jami
+#Period 1 and 2
+#Amrch18;2025
+import turtle
+
+def draw(t, length, n):
+    if n == 0:
+        return
+    angle = 50
+    t.forward(length * n)
+    t.left(angle)
+    draw(t, length, n-1)
+    t.right(2 * angle)
+    draw(t, length, n-1)
+    t.left(angle)
+    t.backward(length * n)
+
+def polygon(t, length, n):
+  if n == 0:
+      return
+  angle = 360 / n
+  for i in range(n):
+        t.forward(length)
+        t.left(angle)
+        
+
+def square(t, length):
+  for i in range(4):
+        t.forward(length)
+        t.left(angle)
+bob = turtle.Turtle()
+
+
+draw(bob, 10, 4)  
+
+#no overlapping
+bob.penup()
+bob.goto(-50, -50) #diff pos
+bob.pendown()
+
+
+polygon(bob, 10, 5)
+square(bob, 90)
+
+
+turtle.done()
